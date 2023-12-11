56e8504da87b511767ab40e5b6453f663d21581e
Danram <dos03042003@gmail.com>
2023-12-11 17:26:11 -0300

    deu certo para inserir eventos no banco

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
 const port = 3001;
@@ -23,12 +24,16 @@ connection.connect(err => {
   }
 });
 
-// Serve arquivos estáticos do diretório 'public'
-app.use(express.static('public'));
-
 // Middleware para processar dados do corpo das requisições
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Configuração da sessão
+app.use(session({
+  secret: 'suaChaveSecreta',
+  resave: false,
+  saveUninitialized: true,
+}));
 
 // Rota para salvar usuário host
 app.post('/api/userhost', (req, res) => {
@@ -40,29 +45,64 @@ app.post('/api/userhost', (req, res) => {
       return res.status(500).json({ message: 'Erro no servidor' });
     }
 
+    // Configurar a sessão do usuário
+    req.session.userhostId = results.insertId;
+
     // Redirecionar para '/grupo' após inserção bem-sucedida
-    res.redirect('/login');
+    res.redirect('/grupo');
   });
 });
 
 // API endpoint for user login
 app.post('/login', (req, res) => {
-    const { nome, email } = req.body;
-  
-    // Retrieve user data from the 'users' table
-    connection.query('SELECT * FROM userhost WHERE nome = ? AND senha = ?', [nome, email], (err, results) => {
-      if (err) {
-        return res.status(500).json({ message: 'Erro no servidor' });
-      }
-  
-      if (results.length === 0) {
-        return res.status(401).json({ message: 'Credenciais inválidas' });
-      }
-  
-      // Redirecione o usuário para a página halloween.html
-      res.redirect('/grupo');
-    });
+  const { nome, email } = req.body;
+
+  // Retrieve user data from the 'users' table
+  connection.query('SELECT * FROM userhost WHERE nome = ? AND senha = ?', [nome, email], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: 'Erro no servidor' });
+    }
+
+    if (results.length === 0) {
+      return res.status(401).json({ message: 'Credenciais inválidas' });
+    }
+
+    // Configurar a sessão do usuário
+    req.session.userhostId = results[0].id;
+
+    // Redirecione o usuário para a página de grupo
+    res.redirect('/grupo');
   });
+});
+
+// Middleware para verificar autenticação
+const verificarAutenticacao = (req, res, next) => {
+  // Verificar se o usuário está autenticado
+  if (req.session.userhostId) {
+    // Se estiver autenticado, continue para a próxima rota
+    next();
+  } else {
+    // Se não estiver autenticado, redirecione para a página de login
+    res.redirect('/login');
+  }
+};
+
+// Rota para criar evento
+app.post('/api/criarevento', (req, res) => {
+  const { nome_evento, quantidade, userhost_id } = req.body;
+
+  // Inserir os dados do evento na tabela 'eventos'
+  const sql = 'INSERT INTO events (nome_evento, quantidade, userhost_id) VALUES (?, ?, ?)';
+  connection.query(sql, [nome_evento, quantidade, userhost_id], (err, result) => {
+    if (err) {
+      console.error('Erro ao inserir evento:', err);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+    } else {
+      // Enviar uma resposta de sucesso para o cliente
+      res.status(201).json({ message: 'Evento criado com sucesso' });
+    }
+  });
+});
   
 // Rota para salvar participante
 app.post('/api/participants', (req, res) => {
